7089962fbd1cc5b5615dfc8fa96cdd7a1815bbae
bolatashim <bolat.ashim@gmail.com>
2017-10-28 15:42:49 +0900

    Login and Registration feature with Firebase added

--- js/reglog_users.js
+++ js/reglog_users.js
@@ -9,11 +9,16 @@ $(document).ready(function() {
   };
   firebase.initializeApp(config);
   
-  
+  //TODO: messages checking for validity of e-mail address
+  //      length of pass should be > 6
+
+
   const txtpassword = document.getElementById("password");
   const txtemail = document.getElementById("username");
   const btnlogin = document.getElementById("loginbtn");
+  const btnregister = document.getElementById("registerbtn");
 
+  //login event listener
   btnlogin.addEventListener("click", e => {
     const email = txtemail.value;
     const pass = txtpassword.value;
@@ -23,6 +28,17 @@ $(document).ready(function() {
     window.location="index.html";
   });
 
+  //registration event listener
+  btnregister.addEventListener("click", e => {
+    const email = txtemail.value;
+    const pass = txtpassword.value;
+    const auth = firebase.auth();
+
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise
+      .then(user => window.location="index.html")
+      .catch(e => console.log(e.message));
+  });
 
 
 });
